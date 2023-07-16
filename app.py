b060f83a2050b201744bf9d8e40f0ca471514f5a
Ashi63 <ashish.ashi6388@gmail.com>
2023-07-17 04:08:36 +0530

    updated project

--- app.py
+++ app.py
@@ -1,13 +1,9 @@
 import streamlit as st
-from src.components import predict
-from src.components import model_evalution
+from file_prediction import Prediction
 import pandas as pd
-import pickle
-from src.utils import *
-import manual_prediction
+
 
 st.header('Wine Prediction Application.')
-
 
 
 
